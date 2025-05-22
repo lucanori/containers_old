58ea2a99fbf65d4dbe914fbf22786a4baa347577
lucanori <github.uncaring594@passmail.net>
2025-05-22 15:24:54 +0000

    fix: add Docker socket access and validation
    Configures the `nobody` user to be part of the `docker` group using the `DOCKER_GID` environment variable.
    
    The entrypoint script checks for Docker socket accessibility and provides an informative error message if access fails, guiding the user to set the correct `DOCKER_GID`

--- apps/deunhealth/entrypoint.sh
+++ apps/deunhealth/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC2086
 
+if [ -S /var/run/docker.sock ]; then
+    if ! docker ps >/dev/null 2>&1; then
+        echo "ERROR: Cannot access Docker socket. Please set the correct DOCKER_GID environment variable."
+        echo "You can find your Docker GID by running 'getent group docker' on your host system."
+        echo "Current DOCKER_GID is set to: ${DOCKER_GID}"
+    fi
+fi
+
 exec /app/main "$@"
